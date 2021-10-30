42b9ef90ad3ffc24b688f34640ae124c261769cb
Kory Erdmann <erdmankp@dukes.jmu.edu>
2021-10-30 01:20:37 +0000

    finish

---
+++ create_tickers.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS tickers;
+DROP USER IF EXISTS tickers_user@localhost;
+
+CREATE DATABASE tickers CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE USER tickers_user@localhost IDENTIFIED WITH mysql_native_password BY '';
+GRANT ALL PRIVILEGES ON tickers.* TO tickers_user@localhost;
